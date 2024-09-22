0ed0a39fbe1b1065bc460b21e7df286527b01b0f
avulanitheesh <ee24mt032@iitdh.ac.in>
2024-09-22 17:26:12 +0530

    added all required port connections in voidinit function

--- main.c
+++ main.c
@@ -5,4 +5,21 @@ void init (void);
 void GPIO_Handler(void);
 void init(void)
 {
+    SYSCTL_RCGCGPIO_R |= 0x20;         // Enable clock for port F
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;    // Unlock GPIO port F
+    GPIO_PORTF_CR_R |= 0x10;           // Commit register for PF4
+
+    GPIO_PORTF_DIR_R = 0x02;           // Set PF1 (LED) as output
+    GPIO_PORTF_DEN_R = 0x12;           // Enable digital function on PF1 and PF4
+
+    GPIO_PORTF_PUR_R = 0x10;           // Enable pull-up on PF4
+
+    GPIO_PORTF_IS_R = 0x00;            // Set edge-sensitive triggering
+    GPIO_PORTF_IBE_R = 0x00;           // Interrupt controlled by IEV
+    GPIO_PORTF_IEV_R = 0x00;           // Falling edge detection for interrupt
+    GPIO_PORTF_ICR_R = 0x10;           // Clear previous interrupt on PF4
+    GPIO_PORTF_IM_R = 0x10;            // Unmask interrupt on PF4
+
+    NVIC_EN0_R = 0x40000000;           // Enable interrupt in NVIC for GPIO port F
+
 }
